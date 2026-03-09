b0a9c80117e9b9b25178e0eda4de58c267f9aa55
jeetestman-creator <jeetestman@gmail.com>
2026-03-09 13:48:30 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './index.css';  // This will work once index.css is added
 import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Toaster position="top-center" />
+    <Toaster position="top-right" />  // Fixed: Added Toaster for notifications
   </React.StrictMode>
 );
