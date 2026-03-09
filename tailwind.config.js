31ff8040b08a4c3dfeca37b004fe2048f0132a6e
jeetestman-creator <jeetestman@gmail.com>
2026-03-09 13:47:00 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ,
-  theme: { extend: },
-  plugins: []
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],  // Fixed: Added standard content paths for scanning
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
